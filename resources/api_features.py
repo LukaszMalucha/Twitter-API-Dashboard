c6d1f2db282eb068467209a2585ab473949bc5b3
Lukasz Malucha <lucasmalucha@gmail.com>
2019-04-18 20:41:51 +0100

    two cities

--- resources/api_features.py
+++ resources/api_features.py
@@ -1,11 +1,11 @@
-from flask import session, Response, render_template, redirect, request
+from flask import Response, render_template, request
 from flask_restful import Resource
 from twitter import twitter_api
 import tweepy
 from operator import itemgetter
 
 
-########################################## City Trends Intersection
+# City Trends Intersection
 
 class CommonTrends(Resource):
 
@@ -41,33 +41,50 @@ class CommonTrends(Resource):
                                             message="Requested ID does not exist, try another one:"))
 
 
-
-
 class RetweetPopularity(Resource):
     """Retweet popularity search"""
-
     def post(self):
         keyword = request.form.get('keyword')
         count = int(request.form.get('count'))
         min_retweets = int(request.form.get('retweets'))
+        counter = count * 4
 
-        ## get tweets for the search query
+        popular_tweets = []
+        for tweet in tweepy.Cursor(twitter_api.search, q=keyword).items(counter):
+            if tweet.retweet_count > min_retweets:
+                dict_ = {"user": tweet.user.name[:25],
+                         "text": tweet.text,
+                         "created_at": str(tweet.created_at),
+                         "retweet_count": tweet.retweet_count,
+                         "keyword": keyword
+                         }
+                popular_tweets.append(dict_)
 
-        results = [status for status in tweepy.Cursor(twitter_api.search, q=keyword).items(count)]
 
-        pop_tweets = [status
-                      for status in results
-                      if status._json['retweet_count'] > min_retweets]
+        tweets_list = popular_tweets[: count]
+        tweets_sorted = sorted(tweets_list, key=itemgetter('retweet_count'), reverse=True)
+        return tweets_sorted
 
-        ## tuple of tweet + retweet count
-        tweet_list = [[tweet._json['text'], tweet._json['created_at'][:19], tweet._json['user']['name'],
-                       tweet._json['retweet_count']]
-                      for tweet in pop_tweets]
 
-        ## sort descending
-        most_popular_tweets = sorted(tweet_list, key=itemgetter(3), reverse=True)[:count]
+
+
+
+        # IF PYTHON LIST NEEDED INSTEAD OF JSON:
+        # results = [status._json for status in tweepy.Cursor(twitter_api.search, q=keyword, min_retweets=50).items(count)]
+        # pop_tweets = [status
+        #               for status in results
+        #               if status._json['retweet_count'] > min_retweets]
+        #
+        # ## tuple of tweet + retweet count
+        # tweet_list = [[tweet._json['text'], tweet._json['created_at'][:19], tweet._json['user']['name'],
+        #                tweet._json['retweet_count']]
+        #               for tweet in pop_tweets]
+        # # tweet_list = {k: v  for v,k in enumerate(pop_tweets)}
+        #
+        #
+        # ## sort descending
+        # most_popular_tweets = sorted(tweet_list, key=itemgetter(3), reverse=True)[:count]
 
         # return Response(
         #     render_template("api_features/most_retweets.html", most_popular_tweets=most_popular_tweets, keyword=keyword,
         #                     count=count, min_retweets=min_retweets))
-        return most_popular_tweets
--- static/js/popular_retweets.js
+++ static/js/popular_retweets.js
@@ -2,6 +2,8 @@ $(document).ready(function() {
 
 
     $('#popularRetweets').on('submit', function(event){
+        $('#tweetColumn').empty();
+        $('.loader').show();
         $.ajax({
             data: {
                 keyword : $('#keyword').val(),
@@ -12,7 +14,23 @@ $(document).ready(function() {
             url : '/retweets'
         })
         .done(function(data){
-                console.log(data);
+            console.log(data);
+            $('.loader').hide();
+            jQuery.each(data, function(index, value){
+                console.log(value.user);
+                console.log(value.text);
+                $('#tweetColumn').append(
+                    '<div class="col-md-6 plain-element">' +
+                    '<a target="_blank" href="https://twitter.com/search?q=' + value.keyword + '&src=typd/">' +
+                    '<div class="card card-tweet"><p>' + value.text + '</p>' +
+                    '<b>&mdash;' +  value.user + '</b> ' + value.created_at +
+                    '<br/> <span><h5><i class="fas fa-retweet"></i> ' +
+                    value.retweet_count +
+                    '</h5></span></div></div>' +
+                    '</a>'
+                )
+
+            });
         });
         event.preventDefault();
 
