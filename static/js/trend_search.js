13be9dc984b1ef44eec5b6fcce8d9b7c8e229b0c
Lukasz Malucha <lucasmalucha@gmail.com>
2019-04-23 16:26:26 +0100

    tweet

--- static/js/trend_search.js
+++ static/js/trend_search.js
@@ -39,7 +39,7 @@ $(document).ready(function() {
             jQuery.each(data, function(index, value){
                 $('#tweetColumn').append(
                     '<div class="col-md-4 plain-element">' +
-                    '<a target="_blank" href="https://twitter.com/search?q=' + value.keyword + '&src=typd/">' +
+                    '<a target="_blank" href="https://twitter.com/search?q=' + value.keyword.slice(1) + '&src=typd/">' +
                     '<div class="card card-tweet"><p>' + value.text + '</p>' +
                     '<b>&mdash;' +  value.user.slice(0,10) + '</b> ' + value.created_at +
                     '<br/> <span><h5><i class="fas fa-retweet"></i> ' +
