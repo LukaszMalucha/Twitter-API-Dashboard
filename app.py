3d8943729afa3c7f6373cbe874144ccdd45fb40d
unknown <lucasmalucha@gmail.com>
2019-06-23 15:45:18 +0100

    blog

--- app.py
+++ app.py
@@ -124,9 +124,9 @@ if __name__ == '__main__':
 
     # app.run()
 
-# Docker
+## Docker
 #     app.run(host='0.0.0.0')
 
-# Heroku
+## Heroku
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
